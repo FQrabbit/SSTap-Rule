8200afee59797a483983261b52fbf869c12a482a
Koinu <serika@milkdog.onmicrosoft.com>
2023-07-21 21:54:21 +0800

    Fix: Wrong filename

--- tools/updategeoip-cn.sh
+++ tools/updategeoip-cn.sh
@@ -3,7 +3,7 @@ echo "#BypassCNandLan,绕过大陆和局域网,1,1,1,0,0,0," >> BypassCNandLan.r
 
 curl https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/CN-ip-cidr.txt >> BypassCNandLan.rules
 
-sed -i '2{/^$/d;}' file.txt # 删除源文件内第二行的空行
+sed -i '2{/^$/d;}' BypassCNandLan.rules # 删除源文件内第二行的空行
 echo 10.0.0.0/8 >> BypassCNandLan.rules
 echo 172.16.0.0/12 >> BypassCNandLan.rules
 echo 192.168.0.0/16 >> BypassCNandLan.rules
