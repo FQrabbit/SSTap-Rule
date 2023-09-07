f732330231321006ffa95bc3526b3fda7d48d115
KoinuDayo <serika@milkdog.onmicrosoft.com>
2023-09-07 22:16:49 +0800

    Chore: Delete unnecessary files, merge them into workflow Fix: Wrong `curl` URL for `someip`

--- tools/updategeoip-cn-someip.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-echo "#BypassCNandLan,绕过大陆和局域网someip,1,1,1,0,0,0," >> BypassCNandLan_someip.rules
-
-curl https://raw.githubusercontent.com/Hackl0us/GeoIP2-CN/release/CN-ip-cidr.txt >> BypassCNandLan_someip.rules
-
-sed -i '2{/^$/d;}' BypassCNandLan_someip.rules # 删除源文件内第二行的空行
-echo 10.0.0.0/8 >> BypassCNandLan_someip.rules
-echo 172.16.0.0/12 >> BypassCNandLan_someip.rules
-echo 192.168.0.0/16 >> BypassCNandLan_someip.rules
--- tools/updategeoip-cn_CDN.bat
+++
@@ -1,8 +0,0 @@
-del BypassCNandLan
-echo #BypassCNandLan,绕过大陆和局域网,1,1,1,0,0,0, >> BypassCNandLan.rules
-
-curl https://cdn.jsdelivr.net/gh/Hackl0us/GeoIP2-CN@release/CN-ip-cidr.txt >> BypassCNandLan.rules
-
-echo 10.0.0.0/8 >> BypassCNandLan.rules
-echo 172.16.0.0/12 >> BypassCNandLan.rules
-echo 192.168.0.0/16 >> BypassCNandLan.rules
